c0b0c13d6dd6c8e43ce918bba5b4b1a0c9814331
hiegova <hiegova@gmail.com>
2019-03-27 18:25:03 +0200

    clear temp files

---
+++ build/precache-manifest.4b030d0c2f3774717550e8aaa37afd44.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "a582fcbd83d19dbd8abab9bfff8ec80a",
+    "url": "/static/media/plus-icon.a582fcbd.svg"
+  },
+  {
+    "revision": "1a4193bb1aac9c3e779a486164bc1b05",
+    "url": "/static/media/minus-icon.1a4193bb.svg"
+  },
+  {
+    "revision": "cbf11813acf8eb0d70d98ebec8e84967",
+    "url": "/static/media/icon.cbf11813.svg"
+  },
+  {
+    "revision": "5d408dca176111d835b533a680428e66",
+    "url": "/static/media/close-icon.5d408dca.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "740e4e90fdf47dfc828e",
+    "url": "/static/js/main.e0e9d9d5.chunk.js"
+  },
+  {
+    "revision": "ab39e54b8989dfe6ccaf",
+    "url": "/static/js/2.fbfb5ebb.chunk.js"
+  },
+  {
+    "revision": "740e4e90fdf47dfc828e",
+    "url": "/static/css/main.47fe600d.chunk.css"
+  },
+  {
+    "revision": "f5d94e7fd8cb4e2042126dd77abcc8a8",
+    "url": "/index.html"
+  }
+];
